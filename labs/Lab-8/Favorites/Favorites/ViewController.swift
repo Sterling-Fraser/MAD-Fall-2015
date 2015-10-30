f98aff7e936365d51b0408572a0e4e20e32e3c59
Sterling-Fraser <Sterling@Sterlings-MacBook-Pro.local>
2015-10-30 16:14:37 -0600

    lab 8
    lab 8

---
+++ labs/Lab-8/Favorites/Favorites/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favorites
+//
+//  Created by Sterling Fraser on 10/20/15.
+//  Copyright (c) 2015 Sterling Fraser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
